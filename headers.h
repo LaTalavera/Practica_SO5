a2735743ae1a9d370a43f4d40ad97e2d6ae2136f
LaTalavera <152596413+LaTalavera@users.noreply.github.com>
2024-12-19 17:07:27 +0100

    Implement all save functions logic

--- headers.h
+++ headers.h
@@ -61,9 +61,11 @@ int RenameFile(EXT_DIRECTORY_ENTRY *directory, EXT_INODE_BLOCK *inodes, char *ol
 int PrintFile(EXT_DIRECTORY_ENTRY *directory, EXT_INODE_BLOCK *inodes, EXT_DATA *data, char *name);
 int DeleteFile(EXT_DIRECTORY_ENTRY *directory, EXT_INODE_BLOCK *inodes, EXT_BYTE_MAPS *byteMaps, EXT_SIMPLE_SUPERBLOCK *superBlock, char *name);
 int CopyFile(EXT_DIRECTORY_ENTRY *directory, EXT_INODE_BLOCK *inodes, EXT_BYTE_MAPS *byteMaps, EXT_SIMPLE_SUPERBLOCK *superBlock, EXT_DATA *data, char *sourceName, char *destName, FILE *file);
+
 void SaveInodesAndDirectory(EXT_DIRECTORY_ENTRY *directory, EXT_INODE_BLOCK *inodes, FILE *file);
 void SaveByteMaps(EXT_BYTE_MAPS *byteMaps, FILE *file);
 void SaveSuperBlock(EXT_SIMPLE_SUPERBLOCK *superBlock, FILE *file);
 void SaveData(EXT_DATA *data, FILE *file);
+void SaveInodeBlock(EXT_INODE_BLOCK *inodeBlock, FILE *file);
 //helper functions
 void DebugListAllDirectoryEntries(EXT_DIRECTORY_ENTRY *directory);
